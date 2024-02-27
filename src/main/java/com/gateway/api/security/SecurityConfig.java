286a8a4da79133a176b92a53930588e43cf1c4e4
Ashwani Kumar <ashwanikumar@Ashwanis-Laptop.local>
2024-02-27 23:06:11 +0530

    Oauth2 support added to gateway by making gateway as resource server in reactive way

---
+++ src/main/java/com/gateway/api/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.gateway.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                ).oauth2ResourceServer((resourceServer) ->
+                        resourceServer.jwt(withDefaults())
+                );
+        return http.build();
+    }
+
+}
